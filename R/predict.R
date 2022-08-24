22ea6abcfe00d804e32f07bbd27b701cbdd40f50
dpweix <dpweix@gmail.com>
2022-08-24 14:32:47 +0000

    Now uses randomForestSRC instead of MultivariateRandomForest package.

--- R/predict.R
+++ R/predict.R
@@ -2,107 +2,108 @@
 #'
 #' Predicts values for new_data using the a trained GRU or MRF method. Also works
 #' for the resMCUSUM method from Bodnar et al (2017) which is based on a VARMA(1, 1)
-#' model. Returns the calculated residuals and the plotting statistic. 
-#' 
+#' model. Returns the calculated residuals and the plotting statistic.
+#'
 #'
 #' @param model Output from the train_gruMCUSUM function.
-#' @param new_data A multivariate time series in dataframe or matrix form. 
+#' @param new_data A multivariate time series in dataframe or matrix form.
 #' @return A named list including the plotting statistic and residuals.
-#' @name predict
-#' @rdname predict
+#' @name predict_fd
+#' @rdname predict_fd
 #' @export
-predict <- function(model, new_data) {
+predict_ml <- function(model, new_data) {
+
   # Constants
   l <- model$constants[2]
-  
+
   # Center and Scale
   if(model$center_scale) {
     new_data <-
-      purrr::pmap_dfc(list(new_data, model$mean_sd$mean, model$mean_sd$sd), \(x, y, z) (x-y)/z) |> 
+      purrr::pmap_dfc(list(new_data, model$mean_sd$mean, model$mean_sd$sd), \(x, y, z) (x-y)/z) |>
       as.matrix()
   }
-  
+
   # Design and Prediction Matrices
   X <- create_X(new_data, lags = l)
   Y <- create_Y(new_data, lags = l)
-  
+
   # Predictions: GRU
   if(grepl("gru", model$method)) {
-    
+
     preds <- pred_gru(model$model, X) # python
     colnames(preds) <- colnames(new_data)
-    
+
     # Predictions: MRF
   } else if(grepl("mrf", model$method)) {
-    preds <- predict_mrf(model$model, X)
+    preds <- randomForestSRC::get.mv.predicted(predict(model$model, as.data.frame(cbind(Y, X))))
     colnames(preds) <- colnames(new_data)
-    
+
     # Predictions: VARMA
   } else if(grepl("var", model$method)) {
     # Extract VAR(1) Model Info
     Ph0 <- model$model$Ph0
     Phi <- model$model$Phi
     Theta <- matrix(0, nrow = dim(Phi)[1], ncol = dim(Phi)[2])
-    
+
     # Convert New Data to Matrix
     new_data <- as.matrix(new_data)
-    
+
     # Prep Residual Matrix
     residuals <- matrix(nrow = nrow(new_data), ncol = ncol(new_data))
-    
+
     # Calc First Row
     pred <- as.numeric(Ph0+Phi %*% as.numeric(tail(model$model$data, 1))-Theta %*% as.numeric(tail(model$model$residuals, 1)))
-    
-    1:ncol(new_data) |> 
+
+    1:ncol(new_data) |>
       purrr::walk(\(j) {
         residuals[1, j] <<- new_data[1, j] - pred[j]
       })
-    
+
     # Calculate Residuals
     2:nrow(new_data) |>
       purrr::walk(\(i) {
-        
+
         pred <- as.numeric(Ph0+Phi %*% new_data[i-1, ]-Theta %*% residuals[i-1, ])
-        
+
         1:ncol(new_data) |>
           purrr::walk(\(j){
             residuals[i,j] <<- new_data[i, j] - pred[j]
           })
       })
-    
+
     # Predictions
     preds <- Y - residuals[-1, ]
-    
+
   } else if(grepl("htsquare", model$method)) {
     preds <- matrix(0, nrow = nrow(Y), ncol = ncol(Y))
   }
-  
-  
+
+
   # Get Residuals
   residuals = Y - preds
-  
+
   # Get Tau
   tau <- calc_tau(residuals)
-  
+
   if(grepl("mcusum", model$method)) {
     # Get S
     S <- calc_S(tau, model$constants[1], model$mu_tau, model$sigma_tau_inv)
-    
+
     # Plotting Statistic
     pstat <- calc_PStat(S, model$sigma_tau_inv)
   } else if(grepl("mewma", model$method)) {
     # Get D
     D <- calc_D(tau, model$mu_tau, model$sigma_tau_inv)
-    
+
     # Plotting Statistic
     pstat <- calc_PStat_MEWMA(model$constants[1], D, model$constants[3])
   } else if(grepl("htsquare", model$method)) {
-    
+
     pstat <- calc_D(tau, model$mu_tau, model$sigma_tau_inv)
   }
-  
-  
-  
+
+
+
   # Return
   list(pstat = pstat,
        residuals = residuals)
@@ -113,7 +114,7 @@ predict <- function(model, new_data) {
 #'
 #' Similar to the build_forest_predict function from MultivariateRandomForest. However,
 #' this function takes in output from build_mrf and makes a new prediction.
-#' 
+#'
 #'
 #' @param model Output from build_mrf.
 #' @param testX The design matrix for predictions. Can be made with create_X.
@@ -125,26 +126,26 @@ predict_mrf <- function (model, testX) {
   # Prediction setup
   Variable_number = model$constants[["p"]]
   n_tree = model$constants[["n_tree"]]
-  
+
   # Set up prediction matrices
   Y_HAT = matrix(0 * (1:Variable_number * nrow(testX)), ncol = Variable_number, nrow = nrow(testX))
   Y_pred = NULL
-                 
+
   # Make predictions
-  1:n_tree |> 
+  1:n_tree |>
    purrr::walk(\(i) {
      # Prediction of single tree
      Y_pred <<- MultivariateRandomForest::single_tree_prediction(model$trees[[i]],
                                                                  testX,
                                                                  Variable_number)
-     
+
      # Add to other tree predictions
-     1:Variable_number |> 
+     1:Variable_number |>
        purrr::walk(\(j) {
          Y_HAT[, j] <<- Y_HAT[, j] + Y_pred[, j]
        })
    })
- 
+
   # Average predictions
   Y_HAT = Y_HAT/n_tree
   return(Y_HAT)
--- R/train.R
+++ R/train.R
@@ -1,10 +1,10 @@
 #' Train methods
 #'
 #' Train the mrfMCUSUM, gruMCUSUM method. Also works for the resMCUSUM method
-#' from Bodnar et al. (2017), which uses a VARMA(1, 1). 
-#' 
+#' from Bodnar et al. (2017), which uses a VARMA(1, 1).
 #'
-#' @param data A multivariate time series in dataframe or matrix form. 
+#'
+#' @param data A multivariate time series in dataframe or matrix form.
 #' @param method An indicator of which model and fault detection method to use. Options are gruMCUSUM, mrfMCUSUM, varMCUSUM, varMEWMA, or htsquare.
 #' @param lags The number of lags of each variable to be included in the design matrix.
 #' @param k A tuning parameter for the MCUSUM, large k results in shorter memory.
@@ -22,14 +22,14 @@ train <- function(data, method = "gruMCUSUM", lags = 1, k = 1.1, r = .3,
   p <- ncol(data)
   mean_sd <- list(mean = colMeans(data),
                   sd = purrr::map_dbl(data, \(x) sd(x)))
-  
+
   # Center and Scale
   if(center_scale) {
-    data <- 
-      purrr::pmap_dfc(list(data, mean_sd$mean, mean_sd$sd), \(x, y, z) (x - y)/z) |> 
+    data <-
+      purrr::pmap_dfc(list(data, mean_sd$mean, mean_sd$sd), \(x, y, z) (x - y)/z) |>
       as.matrix()
   }
-  
+
   # Constant for MCUSUM or MEWMA
   if(grepl("mcusum", method)) {
     constants <- c(k, l, p)
@@ -41,47 +41,56 @@ train <- function(data, method = "gruMCUSUM", lags = 1, k = 1.1, r = .3,
     constants <- c(0, l, p)
     names(constants) <- c("NA", "lags", "p")
   }
-  
+
   # Design and Prediction Matrices
   X <- create_X(data, lags = l)
   Y <- create_Y(data, lags = l)
-  
+
   # Methods: GRU
   if(grepl("gru", method)) {
     fit <- train_gru(X, Y, l) # python
-    
+
     preds <- pred_gru(fit, X) # python
     colnames(preds) <- colnames(data)
-    
+
+    print(c("GRU:", center_scale))
+
   # Methods: MRF
   } else if(grepl("mrf", method)) {
-    fit <-
-      build_mrf(X, Y, n_tree = 10, m_feature = floor(sqrt(l*p)), min_leaf = ceiling(0.01*nrow(X)))
-    
-    preds <- fit$preds
+    fit <- randomForestSRC::rfsrc(formula = as.formula(paste0("Multivar(", paste0(colnames(Y), collapse = ","), ") ~ .")),
+                                  data = as.data.frame(cbind(Y, X)),
+                                  ntree = 500,
+                                  splitrule = "mahalanobis")
+
+
+    preds <- randomForestSRC::get.mv.predicted(fit)
     colnames(preds) <- colnames(data)
-  
+
+    print(c("MRF:", center_scale))
+
   # Methods: VARMA
   } else if(grepl("var", method)) {
     fit <-
       MTS::VAR(data, p = 1, include.mean = TRUE)
-    
+
     preds <- Y - fit$residuals
     colnames(preds) <- colnames(data)
-    
+
+    print(c("VAR:", center_scale))
+
   # Methods: Hotelling's T Square
   } else if(grepl("htsquare", method)) {
     fit <- NA
-    
+
     preds <- matrix(0, nrow = nrow(Y), ncol = ncol(Y))
   }
-  
-  
+
+
   # Get Tau
   tau <- calc_tau(Y - preds)
   mu_tau <- colMeans(tau)
   sigma_tau_inv <- solve(cov(tau))
-  
+
   # Calc Pstat
   if(grepl("mcusum", method)) {
     # MCUSUM
@@ -95,11 +104,13 @@ train <- function(data, method = "gruMCUSUM", lags = 1, k = 1.1, r = .3,
     # Hotelling's T^2
     pstat <- calc_D(tau, mu_tau, sigma_tau_inv)
   }
-  
+
+
+
   # Return
   list(pstat = pstat,
        model = fit,
-       method = method, 
+       method = method,
        residuals = Y - preds,
        center_scale = center_scale,
        mean_sd = mean_sd,
@@ -112,7 +123,7 @@ train <- function(data, method = "gruMCUSUM", lags = 1, k = 1.1, r = .3,
 #'
 #' Similar to the build_forest_predict function from MultivariateRandomForest. However,
 #' this function will save the model for future use.
-#' 
+#'
 #'
 #' @param trainX The design matrix for predictions. Can be made with create_X.
 #' @param trainY The value of the response variables.
@@ -125,16 +136,16 @@ train <- function(data, method = "gruMCUSUM", lags = 1, k = 1.1, r = .3,
 #' @export
 build_mrf <- function (trainX, trainY, n_tree, m_feature, min_leaf) {
   # Stopping conditions
-  if (class(n_tree) == "character" || n_tree%%1 != 0 || 
-      n_tree < 1) 
+  if (class(n_tree) == "character" || n_tree%%1 != 0 ||
+      n_tree < 1)
     stop("Number of trees in the forest can not be fractional or negative integer or string")
-  if (class(m_feature) == "character" || m_feature%%1 != 
-      0 || m_feature < 1) 
+  if (class(m_feature) == "character" || m_feature%%1 !=
+      0 || m_feature < 1)
     stop("Number of randomly selected features considered for a split can not be fractional or negative integer or string")
-  if (class(min_leaf) == "character" || min_leaf%%1 != 
-      0 || min_leaf < 1 || min_leaf > nrow(trainX)) 
+  if (class(min_leaf) == "character" || min_leaf%%1 !=
+      0 || min_leaf < 1 || min_leaf > nrow(trainX))
     stop("Minimum leaf number can not be fractional or negative integer or string or greater than number of samples")
-  
+
   # Bootstrap setup
   theta <- function(trainX) {
     trainX
@@ -142,7 +153,7 @@ build_mrf <- function (trainX, trainY, n_tree, m_feature, min_leaf) {
   results <- bootstrap::bootstrap(1:nrow(trainX), n_tree, theta)
   b = results$thetastar
   Variable_number = ncol(trainY)
-  
+
   # MRF or plain RF
   if (Variable_number > 1) {
     Command = 2
@@ -150,46 +161,46 @@ build_mrf <- function (trainX, trainY, n_tree, m_feature, min_leaf) {
   else if (Variable_number == 1) {
     Command = 1
   }
-  
+
   # Set up prediction matrices
-  Y_HAT = matrix(0 * (1:Variable_number * nrow(trainX)), ncol = Variable_number, 
+  Y_HAT = matrix(0 * (1:Variable_number * nrow(trainX)), ncol = Variable_number,
                  nrow = nrow(trainX))
   Y_pred = NULL
-  
+
   # Build single trees
-  single_trees <- 1:n_tree |> 
+  single_trees <- 1:n_tree |>
     purrr::map(\(i) {
       Single_Model = NULL
       X = trainX[b[, i], ]
       Y = matrix(trainY[b[, i], ], ncol = Variable_number)
       Inv_Cov_Y = solve(cov(Y))
-      
+
       if (Command == 1) {
         Inv_Cov_Y = matrix(rep(0, 4), ncol = 2)
       }
-      
+
       Single_Model = MultivariateRandomForest::build_single_tree(X, Y, m_feature, min_leaf, Inv_Cov_Y, Command)
       Y_pred <<- MultivariateRandomForest::single_tree_prediction(Single_Model, trainX, Variable_number)
-      
-      1:Variable_number |> 
+
+      1:Variable_number |>
         purrr::walk(\(j) {
           Y_HAT[, j] <<- Y_HAT[, j] + Y_pred[, j]
         })
-      
+
       print(paste0("Fit tree ", i))
-      
+
       Single_Model
     })
-  
+
   # Divide by predictions to get average
   Y_HAT = Y_HAT/n_tree
-  
+
   # Return value (for use in predict_mrf)
   list(preds = Y_HAT,
        trees = single_trees,
-       constants = c(n_tree = n_tree, 
+       constants = c(n_tree = n_tree,
                      m_feature = m_feature,
                      min_leaf = min_leaf,
                      p = Variable_number))
-  
+
 }
