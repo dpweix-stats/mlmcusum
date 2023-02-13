1624ab7f604a5018d79dfe6d970552892eb092fc
dpweix <dpweix@gmail.com>
2023-02-13 23:56:19 +0000

    Marked as not working.

--- R/load_python.R
+++ R/load_python.R
@@ -1,9 +1,10 @@
-#' Find python conda location
+#' Find python conda location (DOESN'T WORK)
 #'
-#' Uses reticulate to load in the appropriate conda enviornment. 
+#' DOES NOT WORK.
+#' Uses reticulate to load in the appropriate conda enviornment.
 #' Will then use source_python to load in python source code for
-#' using the GRU. 
-#' 
+#' using the GRU.
+#'
 #'
 #' @param path_conda A path for use_condaenv. This conda enviornment requires numpy and tensorflow.
 #' @param path_py A path for gru_functions.py. This file contains to necessary python code to run the neural networks.
